8ecf60916faa560605d92922e8ee429dc5e0a43b
Dimka Tabakov <nebo.reka@gmail.com>
2021-05-07 11:59:47 +0300

    added required assert output + some fixes

--- tests/conftest.py
+++ tests/conftest.py
@@ -16,17 +16,13 @@ def db_helper(db_path):
 
 @pytest.fixture(scope="session")
 def use_source_db():
-    # source = sqlite3.connect(definitions.DB_PATH)
     source_db = DBHelper(definitions.DB_PATH)
-    print(f'### source DB connected')
     yield source_db.connection
     source_db.close()
-    print(f'### source DB close')
 
 
 @pytest.fixture(scope="session")
 def use_randomized_db(use_source_db):
-    print(f'### use_randomized_db connected')
     source_db = use_source_db
 
     # будем класть копию на диск, будет как доп. лог
@@ -36,55 +32,19 @@ def use_randomized_db(use_source_db):
     randomized_db = DBHelper(definitions.RANDOMIZED_DB_PATH)
     source_db.backup(randomized_db.connection)
 
-    # randomize_ship_part(randomized_db.connection)
+    randomize_ship_part(randomized_db.connection)
     randomize_parts_value(randomized_db.connection)
 
     randomized_db.connection.commit()
 
     yield randomized_db.connection
     randomized_db.close()
-    print(f'### use_randomized_db close')
-
-
-def randomize_using_pandas(dest):
-    df_ships = pd.read_sql_query('SELECT * FROM Ships', dest)
-    df_weapons = pd.read_sql_query('SELECT * FROM weapons', dest)
-    df_hulls = pd.read_sql_query('SELECT * FROM hulls', dest)
-    df_engines = pd.read_sql_query('SELECT * FROM engines', dest)
-    parts = [df_weapons, df_hulls, df_engines]
-
-    columns = df_ships.columns
-    # print(f'columns')
-    # for i in range(0, len(columns)):
-    #     print(f'{i} - {columns[i]}')
-    print('===============================================')
-    # для каждого корабля
-    for i in range(0, df_ships.shape[0]):
-        print(f'====== ship: {df_ships.iat[i, 0]} ======')
-        # генерируем рандомный индекс
-        rand_field_int = random.randint(1, len(columns) - 1)
-        # print(f'rand_field_int: {rand_field_int}')
-
-        # текущее значение поля корабля
-        print(f'value: {df_ships.iat[i, rand_field_int]}')
-
-        # берём по этому же рандомному индексу часть корабля и меняем
-        part = parts[rand_field_int - 1]
-        rand_row = random.randint(0, part.shape[0] - 1)
-        ref = part.iat[rand_row, 0]
-        print(f'ref: {ref}')
-        df_ships.iat[i, rand_field_int] = ref
-
-    df_ships.to_sql('Ships', dest, if_exists='replace')
 
 
 def pytest_generate_tests(metafunc):
-    print(f'### pytest_generate_tests')
     # ищем только нужную фикстуру
-    if 'par_db' in metafunc.fixturenames:
+    if 'parametrize_ships' in metafunc.fixturenames:
         # читаем базы
-        # source_db = sqlite3.connect(definitions.DB_PATH)
-        print(f'## pytest_generate_tests source_db connected')
         source_db = DBHelper(definitions.DB_PATH)
         c = source_db.connection.cursor()
         c.execute(f'SELECT * FROM Ships') # TODO: вынести Ships в константу
@@ -112,15 +72,14 @@ def pytest_generate_tests(metafunc):
                 # print(f'part_name: {part_name}')
                 specific_row_index = part_names.index(part_name)
                 argvalues.append((ship_name[0], part_name, ship_name[specific_row_index+1]))
-                ids.append(f'{ship_name[0]}_{ship_name[specific_row_index+1]}')
+                ids.append(f'{ship_name[0]}, {ship_name[specific_row_index+1]}')
 
         source_db.close()
         print(f'argvalues: {argvalues}')
-        print(f'## pytest_generate_tests source_db close')
 
         if not argvalues:
             raise ValueError("Test cases not loaded")
         # else:
             # print(f'argvalues: {argvalues}')
 
-        return metafunc.parametrize("par_db", argvalues, ids=ids, scope='session')
+        return metafunc.parametrize("parametrize_ships", argvalues, ids=ids, scope='session')
--- tests/test_ships.py
+++ tests/test_ships.py
@@ -1,44 +1,62 @@
+import logging
 import pytest
 
 
-def test_ships(use_source_db, use_randomized_db, par_db):
+def test_ships(use_source_db, use_randomized_db, parametrize_ships):
+    errors = []
+
     source_db = use_source_db
     randomized_db = use_randomized_db
 
     # print(f'par_db: {par_db}')
 
     cs = source_db.cursor()
-    cs.execute(f'SELECT "{par_db[1]}" FROM Ships WHERE ship = "{par_db[0]}"')
+    cs.execute(f'SELECT "{parametrize_ships[1]}" FROM Ships WHERE ship = "{parametrize_ships[0]}"')
     data = cs.fetchone()
     # print(f'data: {data}')
 
     cr = randomized_db.cursor()
-    cr.execute(f'SELECT "{par_db[1]}" FROM Ships WHERE ship = "{par_db[0]}"')
+    cr.execute(f'SELECT "{parametrize_ships[1]}" FROM Ships WHERE ship = "{parametrize_ships[0]}"')
     data_rand = cr.fetchone()
     # print(f'data rand: {data_rand}')
 
     part_source = data[0]
     part_randomized = data_rand[0]
     # print(f'{part_source} -> {part_randomized}')
-    assert part_source == part_randomized, f'{part_source} -> {part_randomized}'
+
+    # if part_source != part_randomized:
+    #     errors.append(f'expected {part_source}, was {part_randomized}')
+    try:
+        assert part_source == part_randomized
+    except AssertionError as e:
+        errors.append(f'expected {part_source}, was {part_randomized}')
+
+    # print(f'argvalue: {par_db[0]}, {par_db[1]}, {par_db[2]}')
+
+    part = parametrize_ships[1]
 
     c_read_columns = source_db.cursor()
-    c_read_columns.execute(f'SELECT name FROM PRAGMA_TABLE_INFO("Ships")')
-    ships_table_columns = c_read_columns.fetchall()
-    ships_table_columns = [x for t in ships_table_columns for x in t]
-    del ships_table_columns[0]
-
-    print(f'argvalue: {par_db[0]}, {par_db[1]}, {par_db[2]}')
-
-    part = par_db[1]
+    c_read_columns.execute(f'SELECT name FROM PRAGMA_TABLE_INFO("{part}s")')
+    part_table_columns = c_read_columns.fetchall()
+    part_table_columns = [x for t in part_table_columns for x in t]
+    del part_table_columns[0]
 
     cs = source_db.cursor()
-    cs.execute(f'SELECT * FROM "{part}s" WHERE "{part}" = "{par_db[2]}"')
+    cs.execute(f'SELECT * FROM "{part}s" WHERE "{part}" = "{parametrize_ships[2]}"')
     value_source = cs.fetchall()
+    print(f'value_source: {value_source}')
 
     cr = randomized_db.cursor()
-    cr.execute(f'SELECT * FROM "{part}s" WHERE "{part}" = "{par_db[2]}"')
+    cr.execute(f'SELECT * FROM "{part}s" WHERE "{part}" = "{parametrize_ships[2]}"')
     value_randomized = cr.fetchall()
+    print(f'value_randomized: {value_randomized}')
 
     for i in range(1, len(value_source[0])):
-        assert value_source[0][i] == value_randomized[0][i], f'{value_source[0][i]} -> {value_randomized[0][i]}'
+        # if value_source[0][i] != value_randomized[0][i]:
+        #     errors.append(f'{part_table_columns[i-1]}: expected {value_source[0][i]}, was {value_randomized[0][i]}')
+        try:
+            assert value_source[0][i] == value_randomized[0][i]
+        except AssertionError as e:
+            errors.append(f'{part_table_columns[i - 1]}: expected {value_source[0][i]}, was {value_randomized[0][i]}')
+
+    assert len(errors) == 0, f'{parametrize_ships[0]}, {parametrize_ships[2]} - {errors}'
